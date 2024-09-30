cd7762d892076b4eebb4d6cf5210b63ec91a2b99
TheHackdes <q0jcgzzjk@mozmail.com>
2024-10-01 00:59:25 +0200

    🐛 Fix bug

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM alpine:latest
 RUN apk add hugo bash git
 
 COPY ./src/init.sh /root/init.sh
+COPY ./src/hugo.yaml /root/hugo.yaml
 RUN chmod +x /root/init.sh
 
 WORKDIR /srv/
--- src/init.sh
+++ src/init.sh
@@ -8,6 +8,8 @@ else
   echo "${WEBSITE} not found"
   hugo new site /srv/${WEBSITE}
   git clone https://github.com/CaiJimmy/hugo-theme-stack /srv/${WEBSITE}/themes/hugo-theme-stack
+  rm -rf /srv/${WEBSITE}/hugo.toml
+  cp /root/hugo.yaml /srv/${WEBSITE}/hugo.yaml
   hugo server --logLevel debug -s /srv/${WEBSITE} --bind 0.0.0.0 --appendPort=false --baseURL="http://127.0.0.1:1313"
 fi
 
